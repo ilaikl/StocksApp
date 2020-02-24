a3310229b8d9df43b5748c1718f8dfd31f405a14
YamOhana <yamoh1993@gmail.com>
2020-02-24 11:38:06 +0200

    API-req

--- server/routes/api.js
+++ server/routes/api.js
@@ -8,51 +8,52 @@ const Recommendation = require('../model/Recommendation.js')
 
 const apiKey = `wViivid8O6bjEmZvBBMWxnMx4E9R2yDbmF2bWYSP5I9Ju1Bygbcp2FH9J7Qt`
 
-const symbols ="'SNAP','TWTR','TEVA','VOD.L'"
+const symbols = "'SNAP','TWTR','TEVA','VOD.L'"
 router.get('/users', async function (req, res) {
     const users = await User.find({})
     console.log(users);
-    
+
     res.send(users)
 })
-router.get('/user/:userId',  function (req, res) {
+router.get('/user/:userId', function (req, res) {
     let userId = req.params.userId
-    User.findOne({userId: req.params.userId}, function (err, user) {
-        res.send(user)   })
-})    
+    User.findOne({ userId: req.params.userId }, function (err, user) {
+        res.send(user)
+    })
+})
 router.get('/stocks', async function (req, res) {
-    request( `https://api.worldtradingdata.com/api/v1/stock?symbol=SNAP,TWTR,VOD.L&api_token=${apiKey}`
+    request(`https://api.worldtradingdata.com/api/v1/stock?symbol=SNAP,TWTR,VOD.L&api_token=${apiKey}`
 
-    ,function(req, response){
-        let stocks = JSON.parse(response.body)
-        console.log(stocks);
-        res.send(stocks)
-    })
+        , function (req, response) {
+            let stocks = JSON.parse(response.body)
+            res.send(stocks)
+        })
 })
+
 router.get('/stock/:stockIdentifier', async function (req, res) {
-    let stockId = req.params.stockIdentifier
-    let Data
-        try { Data = requestPromise(
-            ` https://api.worldtradingdata.com/api/v1/stock?symbol=${stockId}&api_token=${apiKey}`)}
-        catch(err){
-            return
-        }
-    parseString(Data, (err, result) => {
-    Data = result.current //Error : Cannot read property 'current' of undefined
-    Data = [Data]
-    const relevantData = Data.map( t => {
-        return{
-           stockId: t.stockIdentifier[0].$.name,
-           stockName: t.stockIdentifier,
-           currentDate: t.stockIdentifier
-        }
-    })
-})
-})
 
+    let stockId = req.params.stockIdentifier
+    let stockData
+
+    request(` https://api.worldtradingdata.com/api/v1/stock?symbol=${stockId}&api_token=${apiKey}`, function (err, response) {
+
+        if (err) {
+            console.log('error');
+
+        } else {
+            stockData = JSON.parse(response.body)
+            res.send(stockData)
+
+        }
+
+    })
+
+})
 
 router.get('/recommendationSS/:stockSymbol', function (req, res) {
-    Recommendation.find({stockSymbol:req.params.stockSymbol}, function (err, recommendations) {
+    Recommendation.find({ stockSymbol: req.params.stockSymbol }, function (err, recommendations) {
+        console.log(recommendations);
+        
         res.send(recommendations)
     })
 })
@@ -63,7 +64,7 @@ router.get('/recommendations', function (req, res) {
     })
 })
 router.get('/recommendation/:rid', function (req, res) {
-    Recommendation.findOne({recommendationId: req.params.rid}, function (err, recommendation) {
+    Recommendation.findOne({ recommendationId: req.params.rid }, function (err, recommendation) {
         res.send(recommendation)
     })
 })
@@ -74,10 +75,10 @@ router.get('/recommendationUid/:userId', function (req, res) {//check if uid == 
     // })
 })
 
-router.post('/recommendation',  function (req, res) {
+router.post('/recommendation', function (req, res) {
     const rcmnd = new Recommendation(req.body)
-    rcmnd.save()        
-        res.end()
-    })
+    rcmnd.save()
+    res.end()
+})
 
 module.exports = router
